2c5a59907349e58a1af5bc0d9aa82aa854aaff52
Paing Thet Kyaw <150858645+Paing100@users.noreply.github.com>
2025-07-16 14:49:50 +0100

    Issue/3 put logo and change the color upon impact(#4)
    * #3 logo now moving and changing color upon impact
    
    * #3 relocate the png files under assets and extract the javascript function under <script> tag
    
    * #3 initial readme
    
    * #3 initial readme for github

--- DVD/extension.js
+++ DVD/extension.js
@@ -5,12 +5,15 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	context.subscriptions.push(
-		vscode.window.registerWebviewViewProvider(
-			'dvdView', 
-			new DVDViewProvider(context)
-		)
-	);
+	const disposable = vscode.commands.registerCommand('DVDBouncer.runDVDBouncer',
+		() => {
+			vscode.window.registerWebviewViewProvider(
+				'dvdView', 
+				new DVDViewProvider(context)
+			)
+		}
+	)
+	context.subscriptions.push(disposable);
 }
 
 function deactivate() {}
@@ -23,27 +26,58 @@ module.exports = {
 
 class DVDViewProvider {
 	constructor(context) {
-		this.context = context;
+		this.context = context;		
+		this.images = ['dvd_logo_red.png', 'dvd_logo_purple.png', 'dvd_logo_blue.png'];
 	}
 
 	resolveWebviewView(webviewView, context, token) {
-		webviewView.webview.options = {
+
+		const webview = webviewView.webview;
+
+		const imageUris = this.images.map(image => (
+			webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, 'assets/' + image)).toString()
+		));
+
+		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, 'dvdScript.js')).toString();
+
+		webview.options = {
 			enableScripts: true, 
+			localResourceRoots: [this.context.extensionUri],
+
 		};
-		webviewView.webview.html = getWebViewContent();
+		webview.html = getWebViewContent(imageUris, scriptUri);
 	}
 }
 
-function getWebViewContent() {
+function getWebViewContent(imageUris, scriptUri) {
+	const currentImage = imageUris[0];
+	const jsImageArray = JSON.stringify(imageUris);
 	return `
 		<!DOCTYPE html>
 		<html lang="en">
 			<head>
 				<meta charset="UTF-8">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<style>
+					html, body {
+						height: 100%;
+						width: 100%;
+						margin: 0;
+						padding: 0;
+						border: 1px solid white;
+						box-sizing: border-box;
+					}
+					img {
+						position: absolute;
+					}
+				</style>
 			</head>
 			<body>
-    		<p>HELLO!</p>
+				<img id="logo" height="20" width="25" src="${currentImage}"></img>
+				<script>
+					window.images = ${jsImageArray}
+				</script>
+				<script src="${scriptUri}"></script>
 			</body>
 		</html>
 	`;
